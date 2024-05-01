df9de543ef31eceb18365c4a91d6567c7766d6ad
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-05-01 14:31:09 +0600

    initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    // Importing config module
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // Importing mongoDB module
+    MongooseModule.forRoot(process.env.MONGO_DB_URI),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
