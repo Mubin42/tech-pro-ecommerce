8bafb96277d5e0c2bd6fd40e555258b529f78c5b
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-05-20 19:02:40 +0600

    backend fix

--- backend/src/store/store.controller.ts
+++ backend/src/store/store.controller.ts
@@ -1,20 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Brand } from './models/brand.model';
-import { Model } from 'mongoose';
-import { Category } from './models/category.model';
-import { Product } from './models/product.model';
 
 @Controller('store')
-export class StoreController {
-  constructor(
-    @InjectModel(Brand.name) private readonly brandModel: Model<Brand>,
-    @InjectModel(Category.name) private readonly categoryModel: Model<Category>,
-    @InjectModel(Product.name) private readonly productModel: Model<Product>,
-  ) {}
-
-  // Implement the CRUD operations for the Brand model
-  findAllBrands() {
-    return this.brandModel.find().exec();
-  }
-}
+export class StoreController {}
--- backend/src/store/store.service.ts
+++ backend/src/store/store.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Brand } from './models/brand.model';
+import { Model } from 'mongoose';
+import { Category } from './models/category.model';
+import { Product } from './models/product.model';
 
 @Injectable()
-export class StoreService {}
+export class StoreService {
+  constructor(
+    @InjectModel(Brand.name) private readonly brandModel: Model<Brand>,
+    @InjectModel(Category.name) private readonly categoryModel: Model<Category>,
+    @InjectModel(Product.name) private readonly productModel: Model<Product>,
+  ) {}
+
+  // Implement the CRUD operations for the Brand model
+  findAllBrands() {
+    return this.brandModel.find().exec();
+  }
+  createBrand(createBrandDto) {
+    const brand = new this.brandModel();
+    const saved = brand.save();
+
+    return saved;
+  }
+
+  async updateBrand(id: string, updateBrandDto) {
+    const brand = await this.brandModel.findByIdAndUpdate(updateBrandDto);
+    if (!brand) {
+      throw new HttpException('Brand Not Found', HttpStatus.NOT_FOUND);
+    }
+    return brand;
+  }
+
+  async getBrandById(id: string) {
+    const brand = await this.brandModel.findById(id);
+    if (!brand) {
+      throw new HttpException('Brand Not Found', HttpStatus.NOT_FOUND);
+    }
+    return brand;
+  }
+
+  async deleteBrand(id: string) {
+    const brand = await this.brandModel.findById(id);
+    if (!brand) {
+      throw new HttpException('Brand Not Found', HttpStatus.NOT_FOUND);
+    }
+    return brand;
+  }
+}
