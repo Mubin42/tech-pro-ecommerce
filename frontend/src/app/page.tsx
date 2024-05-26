5f332861086ac3ae2df811d43a99fe28762c3201
kazimubin42 <kazimubin.46@gmail.com>
2024-05-26 13:03:54 +0600

    updated codes for the frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,10 @@ import Footer from '@/components/layouts/footer/Footer';
 import Navbar from '@/components/layouts/navbar/Navbar';
 
 export default function Home() {
-	return (
-		<main className='flex min-h-screen flex-col items-center justify-between'>
-			<Navbar />
-			<Footer />
-		</main>
-	);
+  return (
+    <main className='flex flex-col items-center justify-between'>
+      <Navbar />
+      <Footer />
+    </main>
+  );
 }
--- frontend/src/components/layouts/footer/Footer.tsx
+++ frontend/src/components/layouts/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
 type FooterProps = {};
 
@@ -18,8 +18,20 @@ const Footer: FC<FooterProps> = ({}) => {
   // effects
 
   // components
+  const iconSection = <div className='h-[300px] bg-blue-400 w-full'></div>;
 
-  return <div className=" bg-red-500 w-full h-16">Footer</div>;
+  return (
+    <div className='w-full bg-zinc-900'>
+      <div className='p-2 w-full grid-cols-1 max-w-[1440px] mx-auto'>
+        <div className='grid grid-cols-1 lg:grid-cols-3'>
+          <div className='h-[300px] bg-purple-400 w-full'></div>
+          <div className='h-[300px] bg-yellow-400 w-full'></div>
+          <div className='h-[300px] bg-orange-400 w-full'></div>
+        </div>
+        {iconSection}
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
