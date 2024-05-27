1cf0a0faa67157e1e6f58798b465684ec2027aa3
kazimubin42 <kazimubin.46@gmail.com>
2024-05-27 16:55:50 +0600

    updated the category card

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import PageLayout from '@/components/layouts/page-layout/PageLayout';
+import Category from '@/components/sections/category/Category';
 import Hero from '@/components/sections/hero/Hero';
 
 export default function Home() {
   return (
     <PageLayout>
       <Hero />
+      <Category />
     </PageLayout>
   );
 }
---
+++ frontend/src/components/reusable-ui/cards/category-card/CategoryCard.tsx
@@ -0,0 +1,31 @@
+import { Phone } from 'lucide-react';
+import React, { FC } from 'react';
+
+type CategoryCardProps = {};
+
+const CategoryCard: FC<CategoryCardProps> = ({}) => {
+  // api
+
+  // hooks
+
+  // states
+
+  // variables
+
+  // styles
+
+  // functions
+
+  // effects
+
+  // components
+
+  return (
+    <div className=' bg-slate-200 flex flex-col gap-4 p-10 items-center rounded-md'>
+      <Phone className='h-8 w-8' />
+      <h1 className='font-semibold text-center'>Phone</h1>
+    </div>
+  );
+};
+
+export default CategoryCard;
